58425824fa7bc7af2cccbb25a254af989ec70ce4
RamMaths <ramses.hdz30@gmail.com>
2025-07-03 17:42:10 -0600

    Fix web site couldn't be reached using domain

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,7 +17,7 @@ resource "aws_s3_bucket_public_access_block" "portfolio_bucket_pab" {
   bucket = aws_s3_bucket.portfolio_bucket.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
